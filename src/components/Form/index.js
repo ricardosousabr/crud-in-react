2b3423e73686a7889eeb52f119ebb88e0acf3388
ricardo <ricardo.br.pi@gmail.com>
2022-11-24 22:01:26 -0300

    feat: enter the input value on the screen

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,3 +1,28 @@
+import { useState } from "react";
+
 export default function Form() {
-  return <h1>Hello Word</h1>;
+  const [valueInput, setValueInput] = useState("");
+
+  function insertValue() {
+    const list = document.querySelector("ul");
+    const item = document.createElement("li");
+
+    item.innerHTML = valueInput;
+    list.appendChild(item);
+  }
+
+  return (
+    <>
+      <div>
+        <form>
+          <input type="text" onChange={(e) => setValueInput(e.target.value)} />
+          <button>click</button>
+        </form>
+        <button onClick={insertValue}>click</button>
+      </div>
+      <div>
+        <ul></ul>
+      </div>
+    </>
+  );
 }
